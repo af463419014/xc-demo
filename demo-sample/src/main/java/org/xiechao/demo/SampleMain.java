ff5bef7d880f11a75e43463a05c271e118a777fb
af463419014 <463419014@qq.com>
2016-07-21 11:17:59 +0800

    init some utils

---
+++ demo-sample/src/main/java/org/xiechao/demo/SampleMain.java
@@ -0,0 +1,7 @@
+package org.xiechao.demo;
+
+/**
+ * Created by yp-tc-m-2682 on 16/7/21.
+ */
+public class SampleMain {
+}
---
+++ demo-utils/src/main/java/org/xiechao/demo/utils/U.java
@@ -0,0 +1,39 @@
+package org.xiechao.demo.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * Created by yp-tc-m-2682 on 16/7/21.
+ */
+public class U {
+    public static final Logger LOG= LoggerFactory.getLogger(U.class);
+
+    public static final ObjectMapper OM=new ObjectMapper();
+
+    static {
+        OM.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS"));
+    }
+
+    /**
+     * 返回一个大于等于0,小于a的整数
+     * @param a
+     * @return
+     */
+    public int rand(int a){
+        return  rand(0,a);
+    }
+
+    /**
+     * 返回一个大于等于a,小于b的整数
+     * @param a
+     * @return
+     */
+    public int rand(int a,int b){
+
+        return (int)(a+Math.random()*(b-a));
+    }
+}
